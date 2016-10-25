532ca476da58ce06e62ac5bbade557f106308d96
Elliott McNary <elliottmcnary@gmail.com>
2016-10-24 23:13:25 -0400

    good styling

--- front/src/components/Track/presenter.js
+++ front/src/components/Track/presenter.js
@@ -8,19 +8,19 @@ function Track ({ track = {}, toggleTrack, auth, likeOnSoundCloud }) {
     <div className='track-group'>
       <UpVote track={track} />
       <div className='song-title-div'>
-        <p className='song-title' onClick={() => toggleTrack(track)}>{track.title}</p>
-        <p className='artist-name'><a href={track.artist_permalink} target='_blank'>{track.artist} </a></p>
+        <p className='song-title' onClick={() => toggleTrack(track)}><span>{track.title}</span></p>
+        <p className='artist-name'><a href={track.artist_permalink} target='_blank'><span>{track.artist}</span></a></p>
         { auth.soundcloud ? <FontAwesome onClick={() => likeOnSoundCloud(track.trackId)} className="soundcloud-like" name={auth.soundcloud.favorites.includes(track.trackId) ? 'heart' : 'heart-o'} size='lg' /> : null }
       </div>
       <div className='link-item-right'>
         <div className='posted-by'>
-          <Link to={`/${track.user.username}`}>{track.user.username}</Link>
+          <Link to={`/${track.user.username}`}><span>{track.user.username}</span></Link>
         </div>
         <div className='up-votes'>
-          <p>{track.upvotes} upvotes</p>
+          <p><span>{track.upvotes} upvotes</span></p>
         </div>
         <div className='genre'>
-          <p>genre: {track.genre || 'idk'}</p>
+          <p><span>genre: {track.genre || 'idk'}</span></p>
         </div>
       </div>
     </div>
