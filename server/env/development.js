cdea08600a04a000d44b3782bd1a5009ea0b6768
Elliott McNary <elliottmcnary@gmail.com>
2017-03-25 11:43:55 -0700

    database_URL

--- server/env/development.js
+++ server/env/development.js
@@ -1,5 +1,5 @@
 module.exports = {
-  'DATABASE_URI': 'postgres://localhost:5432/thatshot',
+  'DATABASE_URL': 'postgres://localhost:5432/thatshot',
   'SESSION_SECRET': 'Optimus Prime is my real dad',
   'TWITTER': {
     'consumerKey': 'INSERT_TWITTER_CONSUMER_KEY_HERE',
