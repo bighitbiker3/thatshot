1f13fe4406ae09d603b1370bb3e33dbb4b705c63
Elliott McNary <elliottmcnary@gmail.com>
2017-03-26 18:34:59 -0700

    url encoder

--- front/src/components/Track/presenter.js
+++ front/src/components/Track/presenter.js
@@ -3,6 +3,7 @@ import UpVote from './UpVote'
 import { Link } from 'react-router'
 import FontAwesome from 'react-fontawesome'
 import copy from 'copy-to-clipboard'
+import urlEncoder from 'postman-url-encoder'
 import { SERVER_LOCATION } from '../../constants/server'
 
 function Track ({ track = {}, toggleTrack, auth, likeOnSoundCloud, copyNotification }) {
@@ -11,7 +12,7 @@ function Track ({ track = {}, toggleTrack, auth, likeOnSoundCloud, copyNotificat
       <UpVote track={track} />
       <div className='song-title-div'>
         <p className='song-title' onClick={() => {
-          copy(`${SERVER_LOCATION}/artist/${track.artist}/${track.title}`)
+          copy(urlEncoder.encode(`${SERVER_LOCATION}/artist/${track.artist}/${track.title}`))
           copyNotification()
         }}><span>{track.title}</span></p>
         <p className='artist-name'><Link to={`/artist/${track.artist}`}><span>{track.artist}</span></Link></p>
